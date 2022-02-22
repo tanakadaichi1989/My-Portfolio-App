dd7b257b4581b21bec538ac920d7de6f21e427e4
Daichi Tanaka <tanakadaichi_1989@icloud.com>
2022-02-22 21:25:48 +0900

    Initial Commit

---
+++ Hankyu/HankyuApp.swift
@@ -0,0 +1,17 @@
+//
+//  HankyuApp.swift
+//  Hankyu
+//
+//  Created by 田中大地 on 2022/02/22.
+//
+
+import SwiftUI
+
+@main
+struct HankyuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
