b53c8688a0507b75c24e356cc2cdfd78acab2807
Daichi Tanaka <tanakadaichi_1989@icloud.com>
2022-03-01 22:01:33 +0900

    テストコードを追加

--- HankyuTests/HankyuTests.swift
+++ HankyuTests/HankyuTests.swift
@@ -25,6 +25,51 @@ class HankyuTests: XCTestCase {
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
+    
+    func testGetNextStation() throws {
+        var count = 0
+        let stations = ModelData().stations
+        
+        stations.map { station in
+            print("\(count) : \(station)")
+            count += 1
+        }
+        
+        let takarazukaOsakaUmeda = stations[0]
+        let takarazukaNakatsu = takarazukaOsakaUmeda.getNextStation()
+        XCTAssertEqual(takarazukaNakatsu?.id, "takarazuka02")
+        
+        let takarazukaTakarazuka = stations[18]
+        XCTAssertEqual(takarazukaTakarazuka.getNextStation()?.id, nil)
+        
+        let kobeOsakaUmeda = stations[19]
+        let kobeNakatsu = kobeOsakaUmeda.getNextStation()
+        XCTAssertEqual(kobeNakatsu?.id, "kobe02")
+        
+        let kobeKobeSannomiya = stations[34]
+        XCTAssertEqual(kobeKobeSannomiya.getNextStation()?.id, nil)
+        
+        let kyotoOsakaUmeda = stations[35]
+        let kyotoJuso = kyotoOsakaUmeda.getNextStation()
+        XCTAssertEqual(kyotoJuso?.id, "kyoto03")
+        
+        let kyotoKyotoKawaramachi = stations[62]
+        XCTAssertEqual(kyotoKyotoKawaramachi.getNextStation()?.id, nil)
+
+    }
+    
+    func testGetPreviousStation() throws {
+        let stations = ModelData().stations
+        
+        let takarazukaOsakaUmeda = stations[0]
+        XCTAssertEqual(takarazukaOsakaUmeda.getPreviousStation()?.id, nil)
+        
+        let takarazukaNakatsu = stations[1]
+        XCTAssertEqual(takarazukaNakatsu.getPreviousStation()?.id, takarazukaOsakaUmeda.id)
+        
+        let takarazukaTakarazuka = stations[18]
+        XCTAssertEqual(takarazukaTakarazuka.getPreviousStation()?.id, "takarazuka55")
+    }
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
@@ -32,5 +77,5 @@ class HankyuTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
 }
