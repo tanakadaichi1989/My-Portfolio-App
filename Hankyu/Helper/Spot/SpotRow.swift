a842364d0c1ff448938dda2ecfd4674ec3a0672c
Daichi Tanaka <tanakadaichi_1989@icloud.com>
2022-03-25 14:02:04 +0900

    GitHub Actions の確認

---
+++ Hankyu/Helper/Spot/SpotRow.swift
@@ -0,0 +1,20 @@
+//
+//  SpotRow.swift
+//  Hankyu
+//
+//  Created by 田中大地 on 2022/03/01.
+//
+
+import SwiftUI
+
+struct SpotRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SpotRow_Previews: PreviewProvider {
+    static var previews: some View {
+        SpotRow()
+    }
+}
---
+++ Hankyu/Model/Spot.swift
@@ -0,0 +1,8 @@
+//
+//  Spot.swift
+//  Hankyu
+//
+//  Created by 田中大地 on 2022/03/01.
+//
+
+import Foundation
